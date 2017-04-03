395d0ccc45c6590b6daa317b0b79f7b8fab0b9ee
Eric Frick <eric.frick@icloud.com>
2017-04-03 19:28:20 -0400

    Adding onboarding layout

---
+++ app/layouts/Onboarding.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  Image,
+  ScrollView,
+  Dimensions
+} from 'react-native';
+
+// Third Part Icons
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MCIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import Images from '../config/images';
+import OnboardingButton from '../components/OnboardButton';
+
+export default class Onboarding extends Component {
+
+    constructor(props) {
+        super(props);
+
+        StatusBar.setHidden(true); // Ian added this comment on Eric's swaggin' new Macbook Pro 13 in which
+        // with the extremely clicky keys.
+
+        this._next = this._next.bind(this);
+        this.index = 0;
+        this.scrollView = 'undefined';
+    }
+
+    _next() {
+        let {height, width} = Dimensions.get('window');
+
+        width *= ++this.index;
+
+        this.scrollView.scrollTo({x: width, y: 0, animated: true});
+    }
+
+    render() {
+        let {height, width} = Dimensions.get('window');
+
+        return (
+            <ScrollView
+                horizontal={true}
+                pagingEnabled={true}
+                showsHorizontalScrollIndicator={false}
+                showsVerticalScrollIndicator={false}
+                removeClippedSubviews={true}
+                scrollEnabled={true}
+                scrollsToTop={false}
+                ref={(component) => {
+                    this.scrollView = component;
+                }}
+            >
+                <View style={[styles.fullScreen, {height, width}]}>
+                    <Image source={Images.moveLogo.source} style={Images.moveLogo.style} />
+                    <OnboardingButton text="Get Started!" onPress={this._next} />
+                </View>
+                <View style={[styles.fullScreen, styles.nextPage, {height, width}]}>
+                    <Text style={styles.header}>Next Page</Text>
+                    <OnboardingButton text="Next" onPress={this._next} />
+                </View>
+                <View style={[styles.fullScreen, styles.lastPage, {height, width}]}>
+                    <Text style={styles.header} onPress={this._next}>Last Page</Text>
+                    <OnboardingButton text="Submit" />
+                </View>
+            </ScrollView>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    fullScreen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'powderblue'
+    },
+    nextPage: {
+        backgroundColor: 'skyblue'
+    },
+    lastPage: {
+        backgroundColor: 'steelblue'
+    },
+    header: {
+        fontFamily: 'Avenir',
+        fontSize: 40,
+        color: '#ffffff',
+        fontWeight: 'bold'
+    }
+});
