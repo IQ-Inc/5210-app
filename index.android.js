9a8ecbc5193d4748b8aa11be9edc2e7c7329ccd1
Ian McIntyre <ianpmcintyre@gmail.com>
2017-04-03 19:44:10 -0400

    Enable onboarding for Android

--- index.android.js
+++ index.android.js
@@ -15,18 +15,20 @@ import {
 // App Imports
 import AndroidTabBar from './app/layouts/AndroidTabBar';
 import Images from './app/config/images';
+import Onboarding from './app/layouts/Onboarding';
 
 export default class codefest extends Component {
 
   render() {
     return (
-      <View style={{flex: 1}}>
-        <View style={{height: 20, backgroundColor: 'powderblue'}} />
-        <View style={{flex: 1, backgroundColor: 'powderblue', alignItems: 'center'}}>
-          <Image source={Images.logo.source} style={Images.logo.style} />
-        </View>
-        <View style={{flex: 3}}><AndroidTabBar style={{backgroundColor: 'powderblue'}}/></View>
-      </View>
+      <Onboarding/>
+      // <View style={{flex: 1}}>
+      //   <View style={{height: 20, backgroundColor: 'powderblue'}} />
+      //   <View style={{flex: 1, backgroundColor: 'powderblue', alignItems: 'center'}}>
+      //     <Image source={Images.logo.source} style={Images.logo.style} />
+      //   </View>
+      //   <View style={{flex: 3}}><AndroidTabBar style={{backgroundColor: 'powderblue'}}/></View>
+      // </View>
     );
   }
 }
